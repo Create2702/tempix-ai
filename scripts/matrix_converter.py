2b38ecb84bfe9a40e7476367de5faacb7c2e51d9
Dmitry Moiseev <149093616+Create2702@users.noreply.github.com>
2026-08-19 16:32:04 +0400

    refactor: Refactor convert() func

--- scripts/matrix_converter.py
+++ scripts/matrix_converter.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import torch
 
 def convert(csv_file_name: str) -> list:
     data = pd.read_csv(csv_file_name)
     clear_data = data.iloc[:, 2:]
-    matrix = [clear_data.values.flatten().tolist()]
+    matrix = torch.tensor(clear_data.values)
     return matrix
