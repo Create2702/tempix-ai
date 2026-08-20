f4aaec7230e850586bbde0043b126b4cfde93d6b
Dmitry Moiseev <149093616+Create2702@users.noreply.github.com>
2026-08-20 18:07:58 +0400

    fix: Fix mistake (return data type error)

--- scripts/matrix_converter.py
+++ scripts/matrix_converter.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import torch
 
-def convert(csv_file_name: str) -> list:
+def convert(csv_file_name: str) -> torch.Tensor:
     data = pd.read_csv(csv_file_name)
     clear_data = data.iloc[:, 2:]
     matrix = torch.tensor(clear_data.values)
